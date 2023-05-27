98e26024f3d88ac3718b18c6d367336aaab9de47
lrshsl <hoeslilars@gmail.com>
2023-05-27 19:40:36 +0200

    Add basic arg paring

---
+++ src/line_input.rs
@@ -0,0 +1,21 @@
+use std::ops::{Generator, GeneratorState};
+
+
+
+
+
+pub struct StringLineInput<'a> {
+    source: &'a str,
+}
+
+impl<'a> Generator<&'a str> for StringLineInput<'a> {
+    type Yield = &'a str;
+    type Return = ();
+
+    fn resume(&mut self, _: &'a str) -> GeneratorState<Self::Yield, Self::Return> {
+        for line in self.source.lines() {
+            yield line;
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+struct Database {
+    name: String,
+    connection: sqlite::Connection,
 }
+
+impl Database {
+    pub fn new(name: String) -> Database {
+        Database {
+            name: name.clone(),
+            connection: sqlite::open(name).unwrap(),
+        }
+    }
+}
+
+
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    match args[1..].iter().map(|s| s.as_str()).collect::<Vec<_>>()[..] {
+        ["add", word, def, "to", set_name] => {
+            add_to_set(set_name, word, def);
+        }
+        _ => {
+            println!("Usage: {} add <word> <definition> to <set_name>", args[0]);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn add_to_set(set_name: &str, word: &str, definition: &str) {
+    println!("adding {}, {} to {}", word, definition, set_name);
+}
+
+fn create_set(db: &mut Database, name: &str) {
+    println!("creating set {}", name);
+}
+
